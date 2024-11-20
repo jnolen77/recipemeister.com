ad3a888e9d488999747f791aa51e7fe438bebb5e
jnolen77 <chefjeremynolen@gmail.com>
2024-11-20 17:13:01 -0500

    update menu styling

--- public_html/config/config.php
+++ public_html/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // config.php
-$stylesheet = "/assets/css/styles.css?v=2.3550"; // Path to your CSS file
+$stylesheet = "/assets/css/styles.css?v=2.3551"; // Path to your CSS file
 
 $website = "Savor Germany";
 
