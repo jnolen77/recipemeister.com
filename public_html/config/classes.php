03ab830e982308e28ee89764d53561660fe7addb
jnolen77 <chefjeremynolen@gmail.com>
2024-11-20 16:57:35 -0500

    added optional menu dropdown functionality

--- public_html/config/classes.php
+++ public_html/config/classes.php
@@ -42,4 +42,33 @@ class Meta {
         return $this->keywords;
     }
 }
+
+function renderDesktopMenu($menuItems) {
+    foreach ($menuItems as $label => $link) {
+        if (is_array($link)) {
+            echo "<li class='dropdown'>$label<ul class='dropdown-menu'>";
+            foreach ($link as $subLabel => $subLink) {
+                echo "<li><a href='$subLink'>$subLabel</a></li>";
+            }
+            echo "</ul></li>";
+        } else {
+            echo "<li><a href='$link'>$label</a></li>";
+        }
+    }
+}
+
+// Function to render the sidenav menu
+function renderSidenavMenu($menuItems) {
+    foreach ($menuItems as $label => $link) {
+        if (is_array($link)) {
+            echo "<a href='javascript:void(0)'>$label</a>";
+            foreach ($link as $subLabel => $subLink) {
+                echo "<a href='$subLink' class='submenu'>$subLabel</a>";
+            }
+        } else {
+            echo "<a href='$link'>$label</a>";
+        }
+    }
+}
+
 ?>
--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -1,5 +1,6 @@
 <?php 
 include __DIR__ . '/../config/config.php';
+include __DIR__ . '/../config/classes.php';
 ?>
 
 <!DOCTYPE html>
@@ -21,32 +22,28 @@ include __DIR__ . '/../config/config.php';
     </head>
     
     <body>
-        <header class="d-none d-lg-block custom-header">
+    <header class="d-none d-lg-block custom-header">
             <div class="header-content">
-                <a href="https://recipemeister.com" class="logo">Savor Germany</a>
+                <a href="/" class="logo">Savor Germany</a>
                 <ul class="nav-list">
-                    <li><a href="https://recipemeister.com">Home</a></li>
-                    <li><a href="/2025-tours">2025 Tours</a></li>
-                    <li><a href="/your-hosts">Your Hosts</a></li>
-                    <li><a href="/gallery">Gallery</a></li>
-                    <li><a href="/contact-us">Contact Us</a></li>
+                    <?php renderDesktopMenu($menuItems); ?>
                 </ul>
             </div>
         </header>
-        
-            <!--New navbar-->
-            <div id="mySidenav" class="sidenav">
-                <img src="/assets/images/savor-germany-icon.png" alt="savor germany" style="width:120px; padding-left:20px;">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <a href="/2025-tours">2025 Tours</a>
-                <a href="/your-hosts">Your Hosts</a>
-                <a href="/gallery">Gallery</a>
-                <a href="/contact-us">Contact Us</a>
-              </div>
 
-              <span style="margin:10px;font-size:30px;cursor:pointer;" onclick="openNav()" class="mobile-menu"> 
-                <div class="mobile-title" style="padding: 0px 10px;display:flex;;font-size:22px;color:black;text-align:left;">Savor Germany</div> 
-                <div class="hamburger-menu" style="padding-left:150px;display:flex;"><img src="/assets/images/hamburger-icon.svg" alt="hamburger icon" style="width:40px;"> </div>
-            </span>
-                
-            <!--End New navbar-->
+        <!-- New Navbar -->
+        <div id="mySidenav" class="sidenav">
+            <img src="/assets/images/savor-germany-icon.png" alt="savor germany" style="width:120px; padding-left:20px;">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <?php renderSidenavMenu($menuItems); ?>
+        </div>
+
+        <span style="margin:10px;font-size:30px;cursor:pointer;" onclick="openNav()" class="mobile-menu"> 
+            <div class="mobile-title" style="padding: 0px 10px; display: flex; font-size: 22px; color: black; text-align: left;">
+                Savor Germany
+            </div> 
+            <div class="hamburger-menu" style="padding-left: 150px; display: flex;">
+                <img src="/assets/images/hamburger-icon.svg" alt="hamburger icon" style="width: 40px;"> 
+            </div>
+        </span>
+        <!-- End New Navbar -->
