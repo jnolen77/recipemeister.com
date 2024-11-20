350ae66182477ab9e039bf4ad8a2918cf417bab9
jnolen77 <chefjeremynolen@gmail.com>
2024-11-20 16:21:33 -0500

    added stylesheet to config file

--- public_html/config/config.php
+++ public_html/config/config.php
@@ -1,8 +1,7 @@
 <?php
 // config.php
-$stylesheet = "/assets/css/styles.css?v=2.405"; // Path to your CSS file
+$stylesheet = "/assets/css/styles.css?v=2.3549"; // Path to your CSS file
 
 $website = "Savor Germany";
-
 
 ?>
