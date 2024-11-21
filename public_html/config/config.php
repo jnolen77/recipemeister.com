e5bf4f5fe85586b2468c7621ea67a4f5ad988f81
jnolen77 <chefjeremynolen@gmail.com>
2024-11-21 12:27:56 -0500

    update stylesheet version

--- public_html/config/config.php
+++ public_html/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // config.php
-$stylesheet = "/assets/css/styles.css?v=2.3574"; // Path to your CSS file
+$stylesheet = "/assets/css/styles.css?v=2.3575"; // Path to your CSS file
 
 $website = "Savor Germany";
 
