70c3fcd3ea4cdac75999e24a31bbf334a217cc89
jnolen77 <chefjeremynolen@gmail.com>
2024-11-21 08:21:44 -0500

    added function to js file

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,1 +1,20 @@
 // Toggle the mobile menu on hamburger click
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyImages = document.querySelectorAll("img.lazy");
+
+    const lazyLoad = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.classList.remove("lazy");
+                observer.unobserve(img);
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(lazyLoad);
+    lazyImages.forEach(img => observer.observe(img));
+});
