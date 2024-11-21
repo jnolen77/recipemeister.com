ff74b192acf8d9a58763179797b403b94082cea1
jnolen77 <chefjeremynolen@gmail.com>
2024-11-21 17:16:00 -0500

    removed dropdown menu

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -18,7 +18,7 @@ include __DIR__ . '/../config/classes.php';
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    
+        
     </head>
     
     <body>
@@ -27,10 +27,7 @@ include __DIR__ . '/../config/classes.php';
         <a href="https://recipemeister.com" class="logo">Savor Germany</a>
         <ul class="nav-list">
             <li><a href="https://recipemeister.com">Home</a></li>
-            <li class="hover-link"><a class="hover-link" href="/2025-tours">2025 Tours</a>
-        <ul class="hover-menu">
-            <li class="hover-menu"><a href="bavarian-breweries">Bavarian Breweries</a></li>
-        </ul></li>
+            <li><a href="/2025-tours">2025 Tours</a>
             <li><a href="/your-hosts">Your Hosts</a></li>
             <li><a href="/gallery">Gallery</a></li>
             <li><a href="/contact-us">Contact Us</a></li>
