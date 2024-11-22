90339fc0831675204f9c6908a223acdcf92dd3cb
jnolen77 <chefjeremynolen@gmail.com>
2024-11-22 13:08:07 -0500

    updated header with dropdown menu

--- public_html/includes/header-old.php
+++ public_html/includes/header-old.php
@@ -18,32 +18,22 @@ include __DIR__ . '/../config/classes.php';
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        
-        <style>
-
-</style>
-
+    
     </head>
     
     <body>
     <header class="d-none d-lg-block custom-header">
-        <div class="header-content">
-                <a href="https://recipemeister.com" class="logo">Savor Germany</a>
-                    <div class="dropdown">
-                        <div class="dropbtn"><a href="https://recipemeister.com" class="menu-items">2025 Tours</a></div>
-                        <div class="dropdown-content">
-                            <a href="/springfest-2025" class="dropdown-menu-items">Bavaria & Franconia 2025</a>
-                            <a href="/oktoberfest-2025" class="dropdown-menu-items">Oktoberfest Munich 2025</a>
-                            <a href="/oktoberfest-beyond-2025" class="dropdown-menu-items">Oktoberfest & Beyond 2025</a>
-                        </div>
-                    </div>
-                <a href="/gallery" class="menu-items">Gallery</a>
-                <a href="/your-hosts" class="menu-items">Your Hosts</a>
-                <a href="/contact-us" class="menu-items">Contact</a>
-        </div>
-            
-           
-    </header>
+    <div class="header-content">
+        <a href="https://recipemeister.com" class="logo">Savor Germany</a>
+        <ul class="nav-list">
+            <li><a href="https://recipemeister.com">Home</a></li>
+            <li><a href="/2025-tours">2025 Tours</a></li>
+            <li><a href="/your-hosts">Your Hosts</a></li>
+            <li><a href="/gallery">Gallery</a></li>
+            <li><a href="/contact-us">Contact Us</a></li>
+        </ul>
+    </div>
+</header>
 
         
             <!--New navbar-->
