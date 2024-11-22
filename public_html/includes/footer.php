2a13266904f9cf509dc3bba3c7f553983fb7569a
jnolen77 <chefjeremynolen@gmail.com>
2024-11-22 13:53:11 -0500

    updated files on server

--- public_html/includes/footer.php
+++ public_html/includes/footer.php
@@ -1,8 +1,7 @@
-
 <footer class="footer">
     <div class="footer-column tac">
         <h3>About Us</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.</p>
+        <p>Savor Germany was founded by award winning chefs Jeremy and Jessica Nolen. Experience the best of Germany through its food, beer, wine, and culture.</p>
     </div>
     <div class="footer-column tac">
         <h3>Quick Links</h3>
@@ -22,43 +21,13 @@
     
 </footer>
 
-<script>
-const text = "Willkommen to Savor Germany";
-let index = 0;
 
-function typeWriter() {
-    if (index < text.length) {
-        document.getElementById("dynamicText").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeWriter, 100);
-    }
-}
-</script>
 
-<!--JQUERY CDN LINK-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+4Z2u6I4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script src="/assets/js/scripts.js"></script>
 
-
-<!--WOW.JS LINK-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" ></script>
-<script src="/assets/js/script.js"></script>
-<script>
-new WOW().init();
-</script>
-
-<!--New Navbar script-->
-<script>
-function openNav() {
-    document.getElementById("mySidenav").classList.add("open");
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").classList.remove("open");
-}
-</script>
-<!--End new navbar script-->
 
 
 </body>
--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -11,7 +11,6 @@ include __DIR__ . '/../config/classes.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php echo $description ?? $defaultDescription; ?>">
         <meta name="keywords" content="<?php echo $keywords ?? $defaultKeywords; ?>">
-        <!--ANIMATE CSS LINK (REQUIRED)-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo $stylesheet; ?>">
@@ -19,9 +18,7 @@ include __DIR__ . '/../config/classes.php';
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         
-        <style>
-
-</style>
+    
 
     </head>
     
@@ -29,6 +26,7 @@ include __DIR__ . '/../config/classes.php';
     <header class="d-none d-lg-block custom-header">
         <div class="header-content">
                 <a href="https://recipemeister.com" class="logo">Savor Germany</a>
+                <div style="width:250px;"></div>
                     <div class="dropdown">
                         <div class="dropbtn"><a href="https://recipemeister.com" class="menu-items">2025 Tours</a></div>
                         <div class="dropdown-content">
@@ -46,7 +44,7 @@ include __DIR__ . '/../config/classes.php';
     </header>
 
         
-            <!--New navbar-->
+            <!--Mobile Navbar-->
             <div id="mySidenav" class="sidenav">
                 <img src="/assets/images/savor-germany-icon.png" alt="savor germany" style="width:120px; padding-left:20px;">
                 <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
@@ -61,4 +59,4 @@ include __DIR__ . '/../config/classes.php';
                 <div class="hamburger-menu" style="padding-left:150px;display:flex;"><img src="/assets/images/hamburger-icon.svg" alt="hamburger icon" style="width:40px;"> </div>
             </span>
                 
-            <!--End New navbar-->
+            
