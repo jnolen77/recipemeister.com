1142908c3c5ec3535f2a65574d34e9491c45b59d
jnolen77 <chefjeremynolen@gmail.com>
2024-11-26 10:14:47 -0500

    added social media icons to header

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -24,7 +24,7 @@ include __DIR__ . '/../config/classes.php';
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
- 
+        <script src="https://kit.fontawesome.com/ff6272fd1c.js" crossorigin="anonymous"></script>
         <script>
   (function(w, d, t, h, s, n) {
     w.FlodeskObject = n;
@@ -65,6 +65,10 @@ include __DIR__ . '/../config/classes.php';
                 <a href="/gallery" class="menu-items">Gallery</a>
                 <a href="/your-hosts" class="menu-items">Your Hosts</a>
                 <a href="/contact-us" class="menu-items">Contact</a>
+                <a href="https://facebook.com/savorgermany" target="_blank" ><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://instagram.com/savorgermany" target="_blank" ><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://tiktok.com/savorgermany" target="_blank" ><i class="fa-brands fa-tiktok"></i></a>
+                <a href="https://youtube.com/savorgermany" target="_blank" ><i class="fa-brands fa-youtube"></i></a>
         </div>
             
            
